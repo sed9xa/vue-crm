c74456706d2518cd2a4c8e96d752b94c07763956
sed9xa <39928395+sed9xa@users.noreply.github.com>
2022-09-05 19:34:20 +0300

    Routing added

---
+++ src/components/UI/sidebar.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <li v-for="link in links" :key="link.url">
+      <router-link
+        active-class="active"
+        :exact="link.exact"
+        class="waves-effect waves-orange pointer"
+        :to="link.url"
+        >{{ link.name }}</router-link
+      >
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { name: "Счет", url: "/", exact: true },
+        { name: "История", url: "/history" },
+        { name: "Планирование", url: "/planning" },
+        { name: "Новая запись", url: "/record" },
+        { name: "Категории", url: "/categories" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.router-link-exact-active{
+    background-color: rgb(241, 241, 241);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  /* {
+  {
     path: '/',
     name: 'home',
-    component: () =>{import('../views/home')}
-  }, */
+    meta: {
+      layout: 'main'
+    },
+    component: () =>import('../views/home')
+  },
   {
     path: '/login',
     name: 'login',
@@ -15,13 +18,61 @@ const routes = [
     component: () =>import('../views/login')
   },
   {
+    path: '/registration',
+    name: 'registration',
+    meta: {
+      layout: 'empty'
+    },
+    component: () => import('@/views/registration')
+  },
+  {
     path: '/categories',
     name: 'categories',
     meta: {
       layout: 'main'
     },
     component: () => import('@/views/categories')
-  }
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('@/views/history')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('@/views/planning')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('@/views/profile')
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('@/views/detail-record')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('@/views/record')
+  },
 ]
 
 const router = createRouter({
