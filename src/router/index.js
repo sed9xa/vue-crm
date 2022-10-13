0583490c5d9f2d42f17f355fc19144ec3389f8bd
sed9xa <39928395+sed9xa@users.noreply.github.com>
2022-10-13 20:58:50 +0300

    routes protection added

--- src/router/index.js
+++ src/router/index.js
@@ -1,83 +1,103 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHistory } from "vue-router";
+import { getAuth } from "firebase/auth";
+import record from "@/store/record";
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     meta: {
-      layout: 'main'
+      layout: "main",
+      auth: true,
     },
-    component: () =>import('../views/home')
+    component: () => import("../views/home"),
   },
   {
-    path: '/login',
-    name: 'login',
+    path: "/login",
+    name: "login",
     meta: {
-      layout: 'empty'
+      layout: "empty",
     },
-    component: () =>import('../views/login')
+    component: () => import("../views/login"),
   },
   {
-    path: '/registration',
-    name: 'registration',
+    path: "/registration",
+    name: "registration",
     meta: {
-      layout: 'empty'
+      layout: "empty",
     },
-    component: () => import('@/views/registration')
+    component: () => import("@/views/registration"),
   },
   {
-    path: '/categories',
-    name: 'categories',
+    path: "/categories",
+    name: "categories",
     meta: {
-      layout: 'main'
+      layout: "main",
+      auth: true,
     },
-    component: () => import('@/views/categories')
+    component: () => import("@/views/categories"),
   },
   {
-    path: '/history',
-    name: 'history',
+    path: "/history",
+    name: "history",
     meta: {
-      layout: 'main'
+      layout: "main",
+      auth: true,
     },
-    component: () => import('@/views/history')
+    component: () => import("@/views/history"),
   },
   {
-    path: '/planning',
-    name: 'planning',
+    path: "/planning",
+    name: "planning",
     meta: {
-      layout: 'main'
+      layout: "main",
+      auth: true,
     },
-    component: () => import('@/views/planning')
+    component: () => import("@/views/planning"),
   },
   {
-    path: '/profile',
-    name: 'profile',
+    path: "/profile",
+    name: "profile",
     meta: {
-      layout: 'main'
+      layout: "main",
+      auth: true,
     },
-    component: () => import('@/views/profile')
+    component: () => import("@/views/profile"),
   },
   {
-    path: '/detail-record',
-    name: 'detail-record',
+    path: "/detail-record",
+    name: "detail-record",
     meta: {
-      layout: 'main'
+      layout: "main",
+      auth: true,
     },
-    component: () => import('@/views/detail-record')
+    component: () => import("@/views/detail-record"),
   },
   {
-    path: '/record',
-    name: 'record',
+    path: "/record",
+    name: "record",
     meta: {
-      layout: 'main'
+      layout: "main",
+      auth: true,
     },
-    component: () => import('@/views/record')
+    component: () => import("@/views/record"),
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const auth = getAuth();
+  const currentUser = auth.currentUser;
+  const requireAuth = to.matched.some((record) => record.meta.auth);
+  if (requireAuth && !currentUser) {
+    next("/login");
+    console.log('object');
+  } else {
+    next();
+  }
+});
+
+export default router;
